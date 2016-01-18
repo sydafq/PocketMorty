0e3b073c21032c4aefb4f3d11a4498b3a9d791c5
Afiq <syd.afiq@hotmail.com>
2016-01-18 16:51:44 +0800

    1 Created a reader.
    2 Moved the file "data" to a scannable location.

--- src/dataMorty.java
+++ src/dataMorty.java
@@ -2,5 +2,18 @@ import java.io.*;
 
 //Class extracts data from a readme file and scans the input on which Morty to find
 public class dataMorty {
-
+	
+	public void readtheFile() throws Exception{
+		
+	FileReader fr = new FileReader("data.txt"); 
+	BufferedReader br = new BufferedReader(fr); 
+	String s; 
+	
+		while((s = br.readLine()) != null) { 
+		System.out.println(s);
+		}
+		
+		fr.close();
+	}
+	
 }
--- src/mainClass.java
+++ src/mainClass.java
@@ -1,4 +1,17 @@
 
 public class mainClass {
+	
+	public static void main(String[] args){
 
+		dataMorty trial = new dataMorty();
+		
+		try {
+			trial.readtheFile();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}	
+	
 }
